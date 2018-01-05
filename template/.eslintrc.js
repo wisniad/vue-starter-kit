b83be886032bc2900708adf5be3dca22c2ea67e1
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-05 20:55:37 +0100

    Add no-shadow rule exceptions

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -26,11 +26,8 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-      vue: 'never'
-    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // disallow reassignment of function parameters
     // disallow parameter object manipulation except for specific exclusions
     'no-param-reassign': ['error', {
@@ -41,12 +38,20 @@ module.exports = {
         'e' // for e.returnvalue
       ]
     }],
+    'no-shadow': ['error', {
+      allow: [
+        'state' // for vuex state
+      ]
+    }],
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never'
+    }],
     // allow optionalDependencies
     'import/no-extraneous-dependencies': ['error', {
       optionalDependencies: ['test/unit/index.js']
     }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // enforce single quotes
     quotes: [ 2, 'single' ],
     // enforce no semicolons
