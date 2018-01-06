42a35c51339644e0a53d3327e8b4061b3a1fc03d
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-06 03:20:17 +0100

    Add Icon component, remove HelloWorld

---
+++ template/src/components/Icon/Icon.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  svg.icon(:class="{ 'icon--pointer': isPointer }", @click="onClick")
+    use(:xlink:href="href", :href="href")
+</template>
+
+<script>
+  export default {
+    name: 'Icon',
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      isPointer: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    computed: {
+      href() {
+        return `#${this.name}`
+      },
+    },
+    methods: {
+      onClick() {
+        this.$emit('click')
+      },
+    },
+  }
+</script>
+
+<style src="./Icon.sass" lang="sass" scoped></style>
--- template/src/views/Home.vue
+++ template/src/views/Home.vue
@@ -1,17 +1,11 @@
 <template lang="pug">
   div.home
     img(src="~assets/logo.svg")
-    HelloWorld
 </template>
 
 <script>
-  import HelloWorld from 'components/HelloWorld'
-
   export default {
     name: 'Home',
-    components: {
-      HelloWorld,
-    },
   }
 </script>
 
