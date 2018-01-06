0c27ad8324097882d41d96e936a0f70f40b07834
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-06 22:49:00 +0100

    Add Promise polyfill

--- template/src/main.js
+++ template/src/main.js
@@ -1,6 +1,9 @@
+import './polyfills'
+/* eslint-disable import/first */
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import { sync } from 'vuex-router-sync'
+/* eslint-enable import/first */
 import App from './App'
 import router from './router'
 import store from './store'
