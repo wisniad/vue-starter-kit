7fbb29957f43b81d6e02ac4ceb17ce78caaef0af
Kamil Borkowski <kamil.borkowski.02@gmail.com>
2018-01-06 03:04:04 +0100

    Add Vuelidate

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import { sync } from 'vuex-router-sync'
 import App from './App'
 import router from './router'
 import store from './store'
+
+Vue.use(Vuelidate)
 
 sync(store, router)
 
